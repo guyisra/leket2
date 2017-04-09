c48d7760fc7f36317e8b88bd667c141fc5d9dd12
Amit Avihud <amit.avihud@klarna.com>
2017-04-09 10:31:56 +0300

    New New New

--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 3001
 
@@ -7,8 +8,9 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 })
+app.use(express.static(path.join(__dirname, 'src/public')))
 
-app.get('/', (req, res) => {
+app.get('/status', (req, res) => {
   res.send('Hello World!')
 })
 
