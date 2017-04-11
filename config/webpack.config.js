2e71648d8e9a9fd648392dbbb3c260dc6029cbe0
Amit Avihud <amit.avihud@klarna.com>
2017-04-11 22:51:56 +0300

    Better handle webpack and api stuff

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,16 +1,31 @@
 const path = require('path');
-
+const webpack = require('webpack')
 module.exports = {
+  devtool: 'source-map',
+  
   entry: './src/frontend/index.js',
+
   output: {
     path: path.resolve('src/public'),
     filename: 'bundle.js'
   },
+
   module: {
     loaders: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.(css|scss)$/, loader: 'style-loader!css-loader?modules,localIdentName=[path][name]--[local]!sass-loader'}
     ]
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000
   }
 }
