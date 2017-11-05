09a96689d598d9684ecf2cf301238422072ab3a6
itamar.ben-zaken <itamar.ben-zaken@klarna.com>
2017-11-05 09:51:13 +0200

    WIP

--- src/server/db/seeders/20170703145450-suppliers-seeder.js
+++ src/server/db/seeders/20170703145450-suppliers-seeder.js
@@ -2,25 +2,11 @@
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('Person', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+    console.log(Sequelize)
+    Sequelize.models.Supplier.create()
   },
 
   down: function (queryInterface, Sequelize) {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('Person', null, {});
-    */
+    return queryInterface.bulkDelete('Suppliers', null, {})
   }
 };
